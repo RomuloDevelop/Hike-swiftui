f07a0c2c4873b2b13935b293e14994c980d1128d
RomuloDevelop <romulocg25@gmail.com>
2023-10-01 11:26:44 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Rómulo Gabriel on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
